61167d42505ce441e62dddb221379380c62afd8f
Muh-Uzair <muhammaduzair1062001@gmail.com>
2025-10-22 00:50:46 +0500

    db try 1

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ mongoose
 
 if (require.main === module) {
   const port = Number(process.env.PORT || 4000);
-  const server = app.listen(port, "localhost", () => {
+  const server = app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
 
