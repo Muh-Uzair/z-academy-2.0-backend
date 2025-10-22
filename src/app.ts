8f3b166e6670dcfe7ee84ef5cd0462a3131f9a2a
Muh-Uzair <muhammaduzair1062001@gmail.com>
2025-10-22 17:32:05 +0500

    try10

--- src/app.ts
+++ src/app.ts
@@ -96,10 +96,6 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.use(() => {
-  console.log("Hello");
-});
-
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/courses", coursesRouter);
